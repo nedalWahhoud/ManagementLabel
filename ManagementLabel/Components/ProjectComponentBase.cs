da7a6e968adc83388411063f67f5835aa4fbc527
nedalWahhoud <nedal.1992.29@gmail.com>
2026-06-16 17:21:24 +0200

    ProjektCompmentBase.cs erstellen und IsReandedPage umsetzen

---
+++ ManagementLabel/Components/ProjectComponentBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ManagementLabel.Components
+{
+    public class ProjectComponentBase : ComponentBase
+    {
+        protected (bool Initialized, bool ParametersSet, bool AfterRender) IsRendered;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+    }
+}
